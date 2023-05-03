d46469df16e3c983e782396b60d9aadff3780b25
Lukas-Nadj <nadjlukas@hotmail.com>
2023-05-04 00:03:44 +0200

    Comitter lige kommentare på koden først
    comments

--- Ardu.ino
+++ Ardu.ino
@@ -1,84 +1,110 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>  // Include the mDNS library
-#include <WiFiManager.h>  
-#include <SPI.h>
-#include <SD.h>
+#include <ESP8266WiFi.h> //wifi kapabilitet
+#include <ESP8266WebServer.h> //wifi webserver, lytter til http og sender hjemmeside tilbage
+#include <ESP8266mDNS.h> //Lokalt domæne f.eks. "esp8266.local"
+#include <WiFiManager.h>  //automatisk skift af wifi mode til AP så man kan indstille wifi.
+#include <SPI.h> //SPI kontrol
+#include <SD.h> //SD Kort funktioner
+
 WiFiManager wifiManager;
-ESP8266WebServer server(80);
+ESP8266WebServer server(80); //tallet '80' er porten det serveres på. http og https fra hjemmesider sendes over port 80, porten er derfor tit forebeholdt til dette. og det er standard port for browseren.
 int timer = 0;
 
-void setup() {
-  Serial.begin(115200);
-  /*WiFi.begin("Test", "boje1234");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Waiting to connect");
-  }*/
-  wifiManager.autoConnect("esp8266 wifimanager");
-  Serial.println(WiFi.localIP());
-  server.on("/", handleRoot);
-  server.on("/data", Data);
-  server.on("/delete", Delete);
-  server.on("/voltagedata", voltagedata);
-  server.enableCORS(true);
+//pointers til aktive forsøgs-fil, og Boolean optagelse
+bool optag = false;
+File loglocation;
 
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS Responder Started");
-    MDNS.addService("http", "tcp", 80);
+
+void setup() {
+  //begynd serial.
+  Serial.begin(115200);
+
+  //Autoconnecter til gemt wifi og sætter et access point op til at skifte wifi ell. skrive information ind.
+  wifiManager.autoConnect("esp8266 wifimanager");
+  Serial.println(WiFi.localIP()); // printer ip til konsol.
+
+  //definere weblokationer på domænet, requests at lytte efter og funktionerne til at håndtere dem.
+  server.on("/", handleRoot);
+  server.on("/data", Data); //håndteres af funktionen "data" 
+  server.on("/toggleforsøg", toggle);
+  server.on("/delete", Delete); //etc
+  server.on("/voltagedata", voltagedata);
+  
+  // Cross Origin Resource Sharing. tilladelse til at hente hjemmesiden fra andre domæner. (det er normalt fra, på mange hjemmesider som YOUTUBE for at beskytte din information)
+  server.enableCORS(true);   if (MDNS.begin("esp8266")) { //er mDNS sat op og klar?
+    Serial.println("MDNS Responder Started"); //JA
+    MDNS.addService("http", "tcp", 80); //lyt på domænet
   } else {
-    Serial.println("MDNS Responder did not start");
+    Serial.println("MDNS Responder did not start"); //NEJ, øv
   }
+  
+  //starter webserveren op.
   server.begin();
+
+  //kører sd.begin som initialisere SD objektet/library, og checker om det lykkes.
   if (!SD.begin(4)) {
-    Serial.println("Card failed, or not present");
+    Serial.println("Card failed, or not present"); 
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Broadcaster og opdatere domænet til ruteren/ap.
   MDNS.update();
-  if (millis() > timer + 10000) {
+  if (millis() > timer + 10000) { //annoncere kun periodisk
     MDNS.announce();
     timer = millis();
   }
-  /*if(digitalRead(D3)==HIGH){
-    Serial.println("pin is high");
-    wifiManager.resetSettings();
-  } else {
-    Serial.println("pin is low");
-  }*/
+
+  if(optag){
+    
+  }
+  
+
+  //tjekker om der er indgående forbindelser og håndtere dem (se server.on() i setup).
   server.handleClient();
 }
-void Data() {
 
+//---------------------FORSØG PÅ SD KORTET------------------
+void Data() { 
+  //åbner data mappen, som er standard lokation for vores forsøg på sd.
   File dir = SD.open("/data/");
-  int numTabs = 2;
 
   String Filer = "";
   while (true) {
-
     File entry = dir.openNextFile();
-    if (!entry) {
-      // no more files
-      break;
+    if (!entry/* Er der ikke flere filer i mappen? */) {
+      break; //stop loop
     }
     if (!entry.isDirectory() && String(entry.name()).charAt(0) != '.' && String(entry.name()).charAt(1) != '_') {
+      /* Filen må ikke være en mappe, og må ikke starte med punktum, da det så ville være en skjult fil, der nogengange skabes for at give mere information om andre filer */
+      /* og må heller ikke starter med _ af samme årsag, nogle SD formateringer skaber kopuier. */
       Filer += String(entry.name()) + String("\n");
     }
   }
   server.send(200, "text/plain", Filer);
 }
-
+//test
 void handleRoot() {
   server.send(200, "text/plain", "bruh");
 }
 
+//---------------------DATALOGGER------------------
 void voltagedata() {
-  server.send(200, "text/plain", String(analogRead(A0) / 320.0, 5));
+  //analogRead læser værdier mellem 0 og 3.2 volt. ved at dividere med 320 ændre fra en værdi mellem 0-1024 til 0 - 3.2 hvilket svarer til volt
+  server.send(200, "text/plain", String(analogRead(A0) / 320.0, 5)); 
+  //sender rå data
 }
 
-void Delete() {
+
+void toggle(){
+  
+
+}
+
+
+
+//---------------------SD KORT------------------
+void Delete() { //SLET FORSØG/FIL
+  //åbner forsøgsmappen
   File dir = SD.open("/data/");
   int numTabs = 2;
   String Filer = "";
@@ -88,10 +114,12 @@ void Delete() {
       // no more files
       break;
     }
-    if (String(entry.name()) == String(server.arg("filename"))) {
-      SD.remove(server.arg("filename"));
-      Serial.println(String("/data/")+String(server.arg("filename")));
+    //sammenligner navnet af filen der skal slettes, med filen vi er nået til
+    if (String(entry.name()) == String(server.arg("filename"))) { 
+      SD.remove(server.arg("filename")); //slet filen med det navn
+      Serial.println(String("/data/")+String(server.arg("filename"))); //print at vi sletter.
     }
   }
   server.send(200, "text/plain", "deleted, " + String(server.arg("filename")));
+  //server.send sender 3 argumenter som følge af http protokollet. 1. statuskode (200 -> OK) 2. dataformat 3. DATA
 }
